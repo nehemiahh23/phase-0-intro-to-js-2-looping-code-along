5f3371f7aa8e65757dc07e1f1108041b574cb974
Nehemiah Henry <nehemiahelihenry@gmail.com>
2023-02-10 04:09:04 -0500

    commit tree 2e711c4ec2ebde610fecfc09d8adf286ba3cdc92

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"]
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    }
+
+    return gifts
+}
+
+function writeCards(names, event) {
+    const thankYous = []
+    for (let i = 0; i < names.length; i++) {
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYous;
+}
+
+console.log(thankYous)
+
+function countDown(int) {
+    while(int >= 0) {
+        console.log(int)
+        int--
+    }
+}
